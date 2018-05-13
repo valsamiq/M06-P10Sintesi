865db3296afbb0ba3c8b980cd4bf9a01a444d4ee
chronos <chronos@DESKTOP-79DUFJC>
2018-05-13 22:00:23 +0200

    Finished!, functional at least (JS)

--- public_html/controller.js
+++ public_html/controller.js
@@ -1,180 +1,163 @@
 window.addEventListener('load', starter, false);
 
 var form = document.forms['addUser'];
-
-var InputName = form['InputName'];
-var nameDiv = document.getElementById('name');
-var checkName = document.getElementById('checkName');
-var showName = document.getElementById('showname');
-
+//Begin declarations about user.
+var InputName = form['inputName'];
 var InputDate = form['inputDate'];
+var nameDiv = document.getElementById('name');
 var birthdateDiv = document.getElementById('birthdate');
-var checkinputDate = document.getElementById('checkinputDate');
-var showBirthdate = document.getElementById('showbirthdate');
-
+var checkName = document.getElementById('checkName');
+var checkDate = document.getElementById('checkDate');
+var showName = document.getElementById('showName');
+var showBirthdate = document.getElementById('showBirthdate');
+//Finish declarations about user.
+//Begin declarations about food(the important think...).
 var SelectFoodBox = document.getElementsByName('food');
 var FoodList = document.getElementById('FoodList');
-var arrayMenjar = ['Fish', 'Chiken', 'Cheese'];
-
-printListMenjar();
-
-//Start CheckProcess
+var foodArrayList = ['Fish', 'Chiken', 'Noodles'];
+//Finish declarations about food(the important think...).
+showFoodArray();
+//CheckProcess: General check on values of general functions.
 function starter() {
     InputName.addEventListener('keyup', checkInputName, false);
     InputDate.addEventListener('keyup', checkInputDate, false);
 }
-
-//Check Name
 function checkForm() {
-    if (checkInputName() && checkInputDate() && checkFood()) {
-        alert("Check the Register Form!");
+    if (checkInputName() && checkInputDate() && checkSelectFood()) {
+        alert("OK Registering Form!");
     } else {
         alert("You should select more than 1 food.");
     }
 }
-
+//Check Name: Check Name format and requierements.
 function checkInputName() {
     var RegExPattern = /^[A-z]{3,15}$/;
-
     if (InputName.value.match(RegExPattern)) {
         nameDiv.setAttribute('class', 'has-success');
-        checkName.innerHTML = 'El nom és correcte';
+        checkName.innerHTML = 'Name format correct.';
         showName.style.color = 'green';
-        showName.innerHTML = '<strong>Nom: </strong>' + InputName.value;
+        showName.innerHTML = '<strong>Name: </strong>' + InputName.value;
         return true;
     } else {
         nameDiv.setAttribute('class', 'has-danger');
-        checkName.innerHTML = 'El nom és incorrecte';
-        showName.innerHTML = '<strong>Nom: </strong>El nom és incorrecte, només accepta entre 3 i 15 lletres';
+        checkName.innerHTML = 'Wrong Name format!';
         showName.style.color = 'red';
+        showName.innerHTML = '<strong>The Name field accepts only between 3 and 15 letters</strong>';
         return false;
     }
 }
-
+//Check Date: Check Date format and requierements.
 function checkInputDate() {
     var PatternDate = /(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/;
-
     if (InputDate.value.match(PatternDate)) {
         birthdateDiv.setAttribute('class', 'has-success');
-        checkinputDate.innerHTML = 'Format data Correcte';
-        showBirthdate.innerHTML = '<strong>Data de naixement: </strong>' + InputDate.value;
+        checkDate.innerHTML = 'Correct Date-Format';
         showBirthdate.style.color = 'green';
+        showBirthdate.innerHTML = '<strong>BirthDate: </strong>' + InputDate.value;
         return true;
     } else {
         birthdateDiv.setAttribute('class', 'has-danger');
-        checkinputDate.innerHTML = 'Format data incorrecte';
-        showBirthdate.innerHTML = 'Format data incorrecte posi un format MM/DD/YYYY';
+        checkDate.innerHTML = 'Wrong Date format!';
         showBirthdate.style.color = 'red';
+        showBirthdate.innerHTML = 'Date format must to be MM/DD/YYYY';
         return false;
     }
 }
-
-function checkFood() {
-    var ItIsOk = false;
+function checkSelectFood() {
+    var EverytingIsGonnaBeOk = false;
     for (var i = 0; i < SelectFoodBox.length; i++) {
         if (SelectFoodBox[i].checked === true) {
-            return true;
+            EverytingIsGonnaBeOk = true;
         }
     }
-    return validat;
+    return EverytingIsGonnaBeOk;
 }
-
 //Start DOM thing
-
-function printListMenjar() {
+function showFoodArray() {
     FoodList.innerHTML = '';
-    for (var i = 0; i < arrayMenjar.length; i++) {
+    for (var i = 0; i < foodArrayList.length; i++) {
 
         var newDiv = document.createElement('DIV');
-        var Borrar = document.createElement('i');
-        var flechaAbajo = document.createElement('i');
-        var flechaArriba = document.createElement('i');
+        var arrowUp = document.createElement('i');
+        var arrowDown = document.createElement('i');
+        var deleteIcon = document.createElement('i');
         var inputCheckbox = document.createElement('input');
 
-        flechaAbajo.style.marginLeft = "5px";
-        flechaArriba.style.marginLeft = "5px";
-        Borrar.style.marginRight = "10px";
+        arrowDown.style.marginLeft = "5px";
+        arrowUp.style.marginLeft = "5px";
+        deleteIcon.style.marginRight = "10px";
         inputCheckbox.style.marginLeft = "15px";
-        Borrar.classList.add('fa', 'fa-ban');
-        flechaAbajo.classList.add('fa', 'fa-arrow-down');
-        flechaArriba.classList.add('fa', 'fa-arrow-up');
+        deleteIcon.classList.add('fa', 'fa-ban');
+        arrowDown.classList.add('fa', 'fa-arrow-down');
+        arrowUp.classList.add('fa', 'fa-arrow-up');
         inputCheckbox.setAttribute('type', 'checkbox');
         inputCheckbox.setAttribute('name', 'food');
-        inputCheckbox.setAttribute('value', arrayMenjar[i]);
+        inputCheckbox.setAttribute('value', foodArrayList[i]);
 
-        Borrar.setAttribute('onclick', 'eliminarMenjar(event)');
-        flechaArriba.setAttribute('onclick', 'pujarMenjar(event)');
-        flechaAbajo.setAttribute('onclick', 'baixarMenjar(event)');
+        deleteIcon.setAttribute('onclick', 'removeFood(event)');
+        arrowUp.setAttribute('onclick', 'goUpFood(event)');
+        arrowDown.setAttribute('onclick', 'goDwnFood(event)');
 
-        newDiv.appendChild(Borrar);
-        newDiv.appendChild(flechaAbajo);
-        newDiv.appendChild(flechaArriba);
+        newDiv.appendChild(deleteIcon);
+        newDiv.appendChild(arrowDown);
+        newDiv.appendChild(arrowUp);
         newDiv.appendChild(inputCheckbox);
-        newDiv.innerHTML += '<span style="margin-left:5px;" >' + arrayMenjar[i] + '</span>';
+        newDiv.innerHTML += '<span>' + foodArrayList[i] + '</span>';
         FoodList.appendChild(newDiv);
     }
 }
-
 function addEvent() {
-    arrayMenjar.push(indicaNouMenjar());
-    printListMenjar();
+    foodArrayList.push(setNewFood());
+    showFoodArray();
 }
-
-function pujarMenjar(e) {
-    var totalMenjar = arrayMenjar.length;
-    arrayMenjar2 = [];
-    var posMenjar = arrayMenjar.indexOf(e.target.parentNode.lastElementChild.textContent);
-    if (posMenjar === 0) {
-        arrayMenjar2 = arrayMenjar;
+function setNewFood() {
+    var addingFood = prompt('Name of the new food to add:');
+    return addingFood;
+}
+function goUpFood(selected) {
+    var foodCountner = foodArrayList.length;
+    foodArrayList2 = [];
+    var arrayPosition = foodArrayList.indexOf(selected.target.parentNode.lastElementChild.textContent);
+    if (arrayPosition === 0) {
+        foodArrayList2 = foodArrayList;
     } else {
-        var menjarReplace = arrayMenjar.splice(posMenjar, 1);
-        var posReplace = posMenjar - 1;
-        for (var i = 0; i < totalMenjar - 1; i++) {
-            if (i === posReplace) {
-                arrayMenjar2.push(menjarReplace[0]);
+        var foodMovement = foodArrayList.splice(arrayPosition, 1);
+        var foodFinta = arrayPosition - 1;
+        for (var i = 0; i < foodCountner - 1; i++) {
+            if (i === foodFinta) {
+                foodArrayList2.push(foodMovement[0]);
             }
-            arrayMenjar2.push(arrayMenjar[i]);
+            foodArrayList2.push(foodArrayList[i]);
         }
     }
-    arrayMenjar = arrayMenjar2;
-    printListMenjar();
+    foodArrayList = foodArrayList2;
+    showFoodArray();
 }
-
-function eliminarMenjar(e) {
-    var posMenjar = arrayMenjar.indexOf(e.path[1].lastElementChild.textContent);
-    arrayMenjar.splice(posMenjar, 1);
-    printListMenjar();
-}
-
-function baixarMenjar(e) {
-    var totalMenjar = arrayMenjar.length;
-    arrayMenjar2 = [];
-    var posMenjar = arrayMenjar.indexOf(e.target.parentNode.lastElementChild.textContent);
-    if (posMenjar === totalMenjar - 1) {
-        arrayMenjar2 = arrayMenjar;
+function goDwnFood(selected) {
+    var foodCountner = foodArrayList.length;
+    foodArrayList2 = [];
+    var arrayPosition = foodArrayList.indexOf(selected.target.parentNode.lastElementChild.textContent);
+    if (arrayPosition === foodCountner - 1) {
+        foodArrayList2 = foodArrayList;
     } else {
-        var menjarReplace = arrayMenjar.splice(posMenjar, 1);
-        var posReplace = posMenjar + 1;
-        console.log(posReplace);
-        console.log(totalMenjar - 1);
-        for (var i = 0; i < totalMenjar - 1; i++) {
-            if (i === posReplace) {
-                arrayMenjar2.push(menjarReplace[0]);
+        var foodMovement = foodArrayList.splice(arrayPosition, 1);
+        var foodFinta = arrayPosition + 1;
+        for (var i = 0; i < foodCountner - 1; i++) {
+            if (i === foodFinta) {
+                foodArrayList2.push(foodMovement[0]);
             }
-            arrayMenjar2.push(arrayMenjar[i]);
+            foodArrayList2.push(foodArrayList[i]);
         }
-        if (posReplace === totalMenjar - 1) {
-            arrayMenjar2.push(menjarReplace[0]);
+        if (foodFinta === foodCountner - 1) {
+            foodArrayList2.push(foodMovement[0]);
         }
-
     }
-    arrayMenjar = arrayMenjar2;
-    printListMenjar();
+    foodArrayList = foodArrayList2;
+    showFoodArray();
 }
-
-
-function indicaNouMenjar() {
-    var nouMenjar = prompt('Índica el nom del nou menjar');
-    return nouMenjar;
+function removeFood(selected) {
+    var arrayPosition = foodArrayList.indexOf(selected.path[1].lastElementChild.textContent);
+    foodArrayList.splice(arrayPosition, 1);
+    showFoodArray();
 }
 //End DOM thing
